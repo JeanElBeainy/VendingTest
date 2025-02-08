9b8b90b2a562816e5dd49f8d212e5b88a9416ed8
Jadfahed224 <jadfahed2005@gmail.com>
2025-02-08 18:59:26 +0200

    Update Drink.java

--- src/main/Drink.java
+++ src/main/Drink.java
@@ -1,5 +1,22 @@
 package main;
 
 public class Drink {
-    public int minimumPrice = 1;
+    String name,price;
+    
+    public String getName() {
+    	return name;
+    }
+    public void setName(String name) {
+    	this.name = name;
+    }
+    public String getPrice() {
+    	return price;
+    }
+    public void setPrice(String price) {
+    	this.price = price;
+    }
+    public Drink (String price,String name){
+    	this.name = name;
+    	this.price = price;
+    }
 }
