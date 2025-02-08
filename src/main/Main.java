1c1e5c437f1f14d1f9834ec843cacb113b886163
Jean <197878093+JeanElBeainy@users.noreply.github.com>
2025-02-08 19:00:23 +0200

    Update Main.java
    Check duplicate & let user choose to claim a receipt.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,8 @@
 package main;
 
 import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.Buffer;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -12,36 +11,78 @@ import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        //put receipts in arrayList<arrayList<....
-        //first error : overwrite the data, couldn't get two new receipt at the same
-        //time. Second is exiting in receipt which threw an error.
-        //Third is now, if user wants receipt, I have to write it again.
-        BufferedWriter writer = new BufferedWriter(new FileWriter("Receipt.txt"));
 
-        //comment the above writer & uncomment the 3 lines to get a copy.
-        //String userHome = System.getProperty("user.home");
-        //Path path = Paths.get(userHome, "Documents", "Receipt.txt"); //can either desktop or documents
-        //BufferedWriter writer = Files.newBufferedWriter(path);//comment to not create txt
-
-
+        ArrayList<ArrayList<String>> arrayLists = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
 
-
         if (input.equals("1")) {
-            VendingMachine item1 = new VendingMachine("coke");
-            new Receipt(writer, item1.productName, item1.getProductPrice(), 1);
-            new Receipt(writer, item1.productName, item1.getProductPrice() + 1, 2);
+            VendingMachine item1 = new VendingMachine("c");
+            ArrayList<String> arrayList = new ArrayList<>(3);
+            arrayList.add(item1.productName);
+            arrayList.add(String.valueOf(item1.productPrice));
+            arrayList.add("1");
+
+            VendingMachine item2 = new VendingMachine("c");
+            ArrayList<String> arrayList1 = new ArrayList<>(3);
+            arrayList1.add(item2.productName);
+            arrayList1.add(String.valueOf(item2.productPrice));
+            arrayList1.add("1");
+
+            VendingMachine item3 = new VendingMachine("c");
+            ArrayList<String> arrayList2 = new ArrayList<>(3);
+            arrayList2.add(item3.productName);
+            arrayList2.add(String.valueOf(item3.productPrice));
+            arrayList2.add("1");
+            if(!checkDuplicate(arrayLists, arrayList)) {
+                arrayLists.add(arrayList);
+            }
+            if(!checkDuplicate(arrayLists, arrayList1)) {
+                arrayLists.add(arrayList1);
+            }
+            if(!checkDuplicate(arrayLists, arrayList2)) {
+                arrayLists.add(arrayList2);
+            }
+            //new Orders(arrayList);
+
+            //new Receipt(writer, item1.productName, item1.getProductPrice(), 1);
+            //new Receipt(writer, item1.productName, item1.getProductPrice() + 1, 2);
         }
-        exit(writer);
 
+        Scanner scanForReceipt = new Scanner(System.in);
+        String r = scanner.nextLine();
 
+        if(r.equals("yes")) {
+            listOrders(arrayLists);
+        }
+    }
 
-
-        //System.out.println("File path: " + path); //To know the location of the path the receipt has been copied
+    public static boolean checkDuplicate(ArrayList<ArrayList<String>> arrayList, ArrayList<String> array) {
+        for (int i = 0; i < arrayList.size(); i++) {
+            if(arrayList.get(i).getFirst().equals(array.get(i))) {
+                String quantity = arrayList.get(i).get(2);
+                int quantityInt = Integer.parseInt(quantity) + 1;
+                arrayList.get(i).set(2, Integer.toString(quantityInt));
+                return true;
+            }
+        }
+        return false;
     }
 
     public static void exit(BufferedWriter writer) throws IOException {
         writer.close();
+    }
+
+    public static void listOrders(ArrayList<ArrayList<String>> arrayList) throws IOException {
+        String userHome = System.getProperty("user.home");
+        Path path = Paths.get(userHome, "Documents", "Receipt.txt");
+        BufferedWriter writer = Files.newBufferedWriter(path);
+        for (int i = 0; i < arrayList.size(); i++) {
+            new Receipt(writer, arrayList.get(i).getFirst(),
+                    Integer.parseInt(arrayList.get(i).get(1)),
+                    Integer.parseInt(arrayList.get(i).get(2)));
+        }
+        System.out.println("File path: " + path);
+        exit(writer);
     }
 }
